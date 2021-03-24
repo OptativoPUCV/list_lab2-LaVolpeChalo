8fe94bf03cb003546679529df74680e7b959a96b
Replit user <replituser@example.com>
2021-03-24 20:21:07 +0000

    new try

--- list.c
+++ list.c
@@ -54,12 +54,22 @@ void * nextList(List * list) {
 }
 
 void * lastList(List * list) {
+  if(list->tail==NULL){
+    return NULL;
+  }
   list->current=list->tail;
     return (void*)list->current->data;
 }
 
 void * prevList(List * list) {
+  if(list->current==NULL){
     return NULL;
+  }
+  if(list->current->prev==NULL){
+    return NULL;
+  }
+  list->current=list->current->prev;
+    return (void*)list->current->data;
 }
 
 void pushFront(List * list, const void * data) {
