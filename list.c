a7b66e207051c908f9ffd6218a467fe66fe74feb
Replit user <replituser@example.com>
2021-03-25 00:06:04 +0000

    new try

--- list.c
+++ list.c
@@ -39,7 +39,7 @@ List * createList() {
 ///En esta función se mueve current al primer elemento de la lista///
 void * firstList(List * list) {
 
-  if(list->head->data==NULL){
+  if(list->head==NULL){
 
     return NULL;
 
